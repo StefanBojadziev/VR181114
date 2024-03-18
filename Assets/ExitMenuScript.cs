3313df633fbf09e3e856758f079cf4cf5eb1043f
stefan <stefanbojadziev@hotmail.com>
2024-03-18 22:48:58 +0100

    Full VR application

---
+++ Assets/ExitMenuScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitMenuScript : MonoBehaviour
+{
+    //funkcija za start na igrata | game start function
+    public void PlayTheGameAgain()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+    //funkcija za kraj na igrata | game exit function
+    public void QuitTheGame()
+    {
+        Debug.Log("Igrata se isklucuva / The gaime is quiting!");
+        Application.Quit();
+    }
+}
---
+++ Assets/MainMenuScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    //funkcija za start na igrata | game start function
+    public void PlayTheGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    //funkcija za kraj na igrata | game exit function
+    public void QuitTheGame()
+    {
+        Debug.Log("Igrata se isklucuva / The gaime is quiting!");
+        Application.Quit();
+    }
+}
